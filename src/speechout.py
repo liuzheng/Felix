ff6ed19a8653ff5b73b8941ec1949fa36a74f13f
dgund <devingund@gmail.com>
2014-12-02 15:51:02 -0500

    Added Flite TTS engine, an alternative to eSpeak

--- src/speechout.py
+++ src/speechout.py
@@ -5,11 +5,14 @@ Devin Gund + deg + Section E
 Encapsulates any text-to-speech applications used with Felix
 
 Utilizes modules:
-    - eSpeak
+    - eSpeak or flite
     - aplay
 
 Information for working with eSpeak provided from:
     http://espeak.sourceforge.net/commands.html
+
+Information for working with flite provided from:
+    http://www.festvox.org/flite/doc/flite_7.html#SEC15
 
 Information for working with aplay provided from:
     http://linux.die.net/man/1/aplay
@@ -40,6 +43,34 @@ class eSpeak(object):
     def play(self, file):
         """
         Plays a sound file using aplay
+        Command syntax and parameters obtained from aplay man page
+        """
+        os.system("aplay -D hw:1,0 %s" % file)
+
+class Flite(object):
+    """
+    eSpeak is a compact open source software speech synthesizer
+    To use this for output, eSpeak must be installed on the intended device
+    """
+    @staticmethod
+    def isInstalled():
+        """
+        Returns if flite is installed by running 'which flite' on command line
+        """
+        return os.system("which flite") == 0
+
+    def speakText(self, text,
+                 conversionParams="-voice kal -o output.wav"):
+        """
+        Plays a sound file created from a string of output text
+        """
+        os.system("echo \"%s\" | flite %s" % (text, conversionParams))
+        self.play("output.wav")
+
+    def play(self, file):
+        """
+        Plays a sound file using aplay
+        Command syntax and parameters obtained from aplay man page
         """
         os.system("aplay -D hw:1,0 %s" % file)
 
@@ -48,9 +79,9 @@ def speechOutEngine():
     Returns a TTS engine for use with Felix
     The list of engines is extendible
     When creating a new class, add it to speechClasses
-    Any TTS class must implement isInstalled(), speakText(), and output()
+    Any TTS class must implement isInstalled(), speakText(), and play()
     """
-    speechClasses = [eSpeak]
+    speechClasses = [eSpeak, Flite]
     for speechClass in speechClasses:
         if speechClass.isInstalled(): return speechClass()
     print "ERROR speechout.py: No text-to-speech engines available"
